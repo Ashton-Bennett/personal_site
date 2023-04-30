f1111240cfe9138032f59f0dabb46557411739b5
Ashton Bennett <ashtonbennett@ashtons-imac.lan>
2023-04-30 08:57:39 -0600

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import { Work_Sans } from "@next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const font = Work_Sans({ subsets: ["latin"] });
 export default function App({ Component, pageProps, router }) {
   return (
     <div className={font.className}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
